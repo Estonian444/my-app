3456d7332e6f879ae486b4c4edcdae61eb957e81
Konstantin Kirillov <kkdeop@gmail.com>
2024-01-05 15:35:14 +0400

    first

---
+++ src/components/Header copy.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Profile.css'
+
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://w7.pngwing.com/pngs/787/486/png-transparent-vector-flame-pentecost-fire-logo-thumbnail.png'/>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Profile = () => {
+    return (
+        <div className='content'>Main content
+            <img src='https://bipbap.ru/wp-content/uploads/2017/04/0_7c779_5df17311_orig.jpg'/>
+        </div>
+    )
+}
+
+export default Profile;
